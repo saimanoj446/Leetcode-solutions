739e667d4c51d94141e23eb8f1f2ea95203052f8
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-01-17 16:28:13 +0530

    Time: 0 ms (100%), Space: 22.5 MB (78.12%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,8 +5,7 @@ public:
         int i=0;
         for(int j=1;j<n;j++){
             if(nums[j]!=nums[i]){
-                nums[i+1]=nums[j];
-                i++;
+                nums[++i]=nums[j];
             }
         }
         return i+1;
