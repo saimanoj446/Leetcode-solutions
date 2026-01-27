7256ca95b11396c1c55e4f060ebbf06f6bddae26
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-01-27 15:06:37 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int ind=-1;
+        int n= nums.size();
+        //finding index where first dip occurs from n-1
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        //no dip is found
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        //now swap ind and least elem > ind, we already have increasig order so no need to sort
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        //now reverse arr[ind] to arr[n]
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
