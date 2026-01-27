a199a1c1db05cb44c737c0dacdf24d327bca5e6b
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-01-27 20:48:24 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    void makerow(int i,vector<vector<int>>& matrix){
-        int n=matrix.size();//rows
-        int m=matrix[0].size();//columns
-        for(int j=0;j<m;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-1e9;
-            }
-        }
-    }
-    void makecol(int j,vector<vector<int>>& matrix){
-        int n=matrix.size();//rows
-        int m=matrix[0].size();//columns
-        for(int i=0;i<n;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-1e9;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();//rows
         int m=matrix[0].size();//columns
+        vector<int> row(n,0);
+        vector<int> col(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    makerow(i,matrix);
-                    makecol(j,matrix);
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==-1e9){
+                if(col[j]==1 || row[i]==1){
                     matrix[i][j]=0;
                 }
             }
