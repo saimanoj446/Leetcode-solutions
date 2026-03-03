d2d423db2780d4d577b93fa95ec4a1c556158fb5
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-03-04 01:06:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.35%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double solve(double x, long n){
+        if(n==0) return 1;
+        double half= solve(x,n/2);
+        if(n%2==0){//even
+            return half*half;
+        }
+        else return half*half*x;
+    }
+    double myPow(double x, int n) {
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return solve(x,N);
+    }
+};
