f2a3f40b4c6236a4d3a16a25480723069a83d705
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-03-23 00:55:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (96.93%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -8,18 +8,13 @@ public:
             }
             return;
         }
-        if (ind > 9) {
-            return;
+        for(int i=ind;i<=9;i++){
+            if(i<=n){
+                temp.push_back(i);
+                solve(i+1,k-1,n-i,temp,ans);
+                temp.pop_back();
+            }
         }
-        //pick
-        if(k>0 && ind<=n){
-            temp.push_back(ind);
-            solve(ind+1,k-1,n-ind,temp,ans);
-            temp.pop_back();
-        }
-        //not pick
-        solve(ind+1,k,n,temp,ans);
-
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
