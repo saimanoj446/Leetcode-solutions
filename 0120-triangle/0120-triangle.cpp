d1a62af9c513383c1ba19ca188d0e34414f43b5c
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-05-27 23:57:09 +0530

    Time: 0 ms (100%), Space: 13.2 MB (51.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int solve(int i,int j, vector<vector<int>>& triangle,vector<vector<int>> &dp){
-        if(i==triangle.size()) return 0;
-        if(dp[i][j]!=1e9) return dp[i][j];
-        int one= solve(i+1,j,triangle,dp);
-        int two= solve(i+1,j+1,triangle,dp);
-        return dp[i][j]=(triangle[i][j]+min(one,two));
-
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
+        int m = triangle.size();//no.of rows
         int n= triangle[m-1].size();
         vector<vector<int>> dp(n,vector<int>(m,1e9));
-        return solve(0,0,triangle,dp);
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=triangle[m-1][i];
+        }
+        //last before row to first row
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j]=(triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]));
+            }
+        }
+        return dp[0][0];
     }
 };
