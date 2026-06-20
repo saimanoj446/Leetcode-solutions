2435ba12e77a710cbbae1355970892e0b08875de
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-06-20 23:28:36 +0530

    Time: 5 ms (78.93%), Space: 44.8 MB (47.48%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(),g.end());
         sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
         int i=0;
         int j=0;
-        int count=0;
         while(i<g.size()&&j<s.size()){
             if(s[j]>=g[i]){
-                count++;
                 i++;
                 j++;
             }
             else j++;
         }
-        return count;
+        return i;
     }
 };
