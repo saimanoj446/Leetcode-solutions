f4a1b24c6ecab9c130891cf45e5c146f5b3b29c6
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-06-25 16:23:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (62.25%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int solve(int i,int start,int end,vector<int> &dp, vector<int> & nums){
-        if(i<start) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick = nums[i]+solve(i-2,start,end,dp,nums);
-        int not_pick = solve(i-1,start,end,dp,nums);
-        return dp[i]=max(pick,not_pick);
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        int case1 = solve(n-1,1,n-1,dp1,nums);
-        int case2 = solve(n-2,0,n-2,dp2,nums);
-        return max(case1,case2);
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        if(n==3) return max({nums[0],nums[1],nums[2]});
+        int prev=nums[0];
+        int curr=max(nums[0],nums[1]);
+        for(int i=2;i<n-1;i++){
+            int pick=nums[i]+prev;
+            int not_pick=curr;
+            int next=max(pick,not_pick);
+            prev=curr;
+            curr=next;
+        }
+        int way1=curr;
+
+        prev=nums[1];
+        curr=max(nums[1],nums[2]);
+        for(int i=3;i<n;i++){
+            int pick=nums[i]+prev;
+            int not_pick=curr;
+            int next=max(pick,not_pick);
+            prev=curr;
+            curr=next;
+        }
+        int way2=curr;
+        return max(way1,way2);
+
     }
 };
