635281c934566a9263faa5da7bcef691f9719d57
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-08 19:48:03 +0530

    Time: 4 ms (14.53%), Space: 16.8 MB (70.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();//rows
-        int n=grid[0].size();//cols
+        int m=grid.size();
+        int n=grid[0].size();
         int freshCount=0;
-        queue<pair<pair<int,int>,int>> q;//<row,col>,time
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                 }
-                else if(grid[i][j]==1){
-                    freshCount++;
-                }
+                if(grid[i][j]==1) freshCount++;
             }
         }
         int maxTime=0;
