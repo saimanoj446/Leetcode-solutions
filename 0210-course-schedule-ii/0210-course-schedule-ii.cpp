0cc1a41053a5adb43f7c060f0d933f175af1bff5
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-12 22:04:20 +0530

    Time: 3 ms (71.05%), Space: 19.1 MB (19.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>>&adj, vector<int> &vis,vector<int> &pathvis,vector<int> &ans){
-        vis[node]=1;
-        pathvis[node]=1;
-        for(int neighbour: adj[node]){
-            if(!vis[neighbour]&&!pathvis[neighbour]){
-                if(dfs(neighbour,adj,vis,pathvis,ans)==true){
-                    return true;
-                }
-            }
-            else if(pathvis[neighbour]){
-                return true;
-            }
-        }
-        pathvis[node]=0;
-        ans.push_back(node);
-        return false;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<vector<int>> adj(n);
+        vector<int> indegree(numCourses,0);
+        vector<int> ans;
+        for(auto it:prerequisites){
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        vector<vector<int>> adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
             int a=prerequisites[i][0];
             int b=prerequisites[i][1];
             adj[b].push_back(a);
         }
-        //converted into adj list
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathvis,ans)==true){
-                    return {};
-                }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;   
+        if(ans.size() == numCourses) {
+            return ans;
+        }
+        return {};
     }
 };
