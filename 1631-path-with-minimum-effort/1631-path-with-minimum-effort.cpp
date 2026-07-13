81e178d7e6cb1560184f0ee4682b1dab1fdd75ff
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-13 22:29:32 +0530

    Time: 56 ms (54.58%), Space: 24.6 MB (43.69%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,32 +3,35 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         dist[0][0]=0;
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        using PIII=pair<int,pair<int,int>>;
+        priority_queue<PIII,vector<PIII>,greater<PIII>> pq;
         pq.push({0,{0,0}});
         while(!pq.empty()){
-            int eff=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
+            auto it=pq.top();
+            int currDist=it.first;
+            int currRow=it.second.first;
+            int currCol=it.second.second;
             pq.pop();
-            int delRow[]={-1, 1, 0, 0};
-            int delCol[]={0, 0, -1, 1};
+            if(currDist<dist[currRow][currCol]) continue;
+            int delRow[]={-1,0,1,0};
+            int delCol[]={0,1,0,-1};
             for(int i=0;i<4;i++){
-                int r=row+delRow[i];
-                int c=col+delCol[i];
-                if(r>=0 && c>=0 && r<n && c<m){
-                    int effort=abs(heights[row][col]-heights[r][c]);
-                    int path= max(eff,effort);
-                    if(path<dist[r][c]){
-                        dist[r][c]=path;
-                        pq.push({path,{r,c}});
+                int nextRow=currRow+delRow[i];
+                int nextCol=currCol+delCol[i];
+                if(nextRow>=0 && nextCol>=0 && nextRow<n && nextCol<m){
+                    int effort=abs(heights[currRow][currCol]-heights[nextRow][nextCol]);
+                    int path=max(effort,currDist);
+                    if(path<dist[nextRow][nextCol]){
+                        dist[nextRow][nextCol]=path;
+                        pq.push({path,{nextRow,nextCol}});
                     }
                 }
             }
         }
-        if(dist[n-1][m-1]<1e9) return dist[n-1][m-1];
-        else return -1;
+        if(dist[n-1][m-1]==INT_MAX)return -1;
+        return dist[n-1][m-1];
 
     }
 };
