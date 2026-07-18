dbd6214e822cf7ed0469a592ef27ea0747419369
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-18 19:07:28 +0530

    Time: 3 ms (88.87%), Space: 23.8 MB (59.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
         vector<vector<int>> ans;
-        int n = intervals.size();
-        for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
             else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                ans.push_back(intervals[i]);
             }
         }
         return ans;
