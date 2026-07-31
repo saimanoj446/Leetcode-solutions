256bfd222d4fb0d05807dcdc2ba6df3b2d312e17
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-31 12:30:29 +0530

    Time: 0 ms (100%), Space: 14.5 MB (89.11%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> mp(26,0);
+        for(int i=0;i<s.size();i++){
+            mp[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
