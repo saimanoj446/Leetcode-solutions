fe25bfe9cd5f07cf7010adc0fd23110b88a37a43
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-07-31 11:59:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        vector<int> mp1(26,0);
+        vector<int> mp2(26,0);
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]-'a']++;
+            mp2[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(mp1[i]!=mp2[i]) return false;
+        }
+        return true;
+    }
+};
