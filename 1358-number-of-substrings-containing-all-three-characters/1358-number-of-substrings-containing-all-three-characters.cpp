7e4f0bfbd2f106f9b5633ceca997727e00ee23b5
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-08-19 23:21:49 +0530

    Time: 18 ms (23.53%), Space: 11.1 MB (28.42%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+    bool isValid(char c){
+        return (c=='a'||c=='b'||c=='c');
+    }
     int numberOfSubstrings(string s) {
-        vector<int> mp(3,0);
+        unordered_map<char,int> last_seen;
         int n=s.size();
-        int l=0;
-        int subarrcnt=0;
-        for(int r=0;r<s.size();r++){
-            mp[s[r]-'a']++;
-            while(mp[0]>=1 && mp[1]>=1 && mp[2]>=1){
-                subarrcnt+=(n-r);
-                mp[s[l]-'a']--;
-                l++;
+        int lastnonvalid=-1;
+        int cnt=0;
+        for(int r=0;r<n;r++){
+            if(!isValid(s[r])){
+                lastnonvalid=r;
+                last_seen.clear();
             }
+            else{
+                last_seen[s[r]]=r;
+                if(last_seen.size()==3){
+                    int min_valid=min({last_seen['a'],last_seen['b'],last_seen['c']});
+                    cnt+=(min_valid-lastnonvalid);
+                }
+            }
+
         }
-        return subarrcnt;
+        return cnt;
     }
 };
