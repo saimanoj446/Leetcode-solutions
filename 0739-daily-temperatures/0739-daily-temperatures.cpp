275fc865a50c442c4ddd61e49770379e037604fd
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-08-20 00:58:01 +0530

    Time: 22 ms (54.79%), Space: 102.9 MB (78.22%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>st;
+        int n=temperatures.size();
+        vector<int> ans(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()){
+                if(temperatures[i]>=temperatures[st.top()]){
+                    st.pop();
+                }
+                else{
+                    ans[i]=st.top()-i;
+                    break;
+                }
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
