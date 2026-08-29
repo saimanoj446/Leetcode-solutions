e69cbaf71d626489a9bc1e2621bc9277cf7eba27
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-08-30 00:38:28 +0530

    Time: 6 ms (47.08%), Space: 10.9 MB (71.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n= s.size();
         vector<int> mp(26,0);
         int l=0;
-        int replace=0;
         int maxcnt=0;
+        int maxi=0;
+        int replace=0;
         for(int r=0;r<s.size();r++){
             mp[s[r]-'A']++;
             maxcnt=max(maxcnt,mp[s[r]-'A']);
-            replace=r-l+1-maxcnt;
-            if(replace>k){
+            while(r-l+1-maxcnt>k){
                 mp[s[l]-'A']--;
                 l++;
             }
-
+            maxi=max(maxi,r-l+1);
         }
-        return s.size()-l;
+        return maxi;
     }
 };
