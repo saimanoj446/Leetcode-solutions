7eeca3fe24c448c037189934b6df4253c94bde45
saimanoj446 <164757464+saimanoj446@users.noreply.github.com>
2026-08-30 00:25:56 +0530

    Time: 50 ms (30.7%), Space: 84.9 MB (34.53%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,7 +6,7 @@ public:
         int maxi=0;
         for(int r=0;r<fruits.size();r++){
             mp[fruits[r]]++;
-            while(mp.size()>2){
+            if(mp.size()>2){
                 mp[fruits[l]]--;
                 if(mp[fruits[l]]==0){
                     mp.erase(fruits[l]);
